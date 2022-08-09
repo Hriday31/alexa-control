06e1336049366ba3c303c7812938d1ccc15638d5
Hriday31 <hriday311203@gmail.com>
2022-08-09 12:52:50 +0530

    Added light on and off

---
+++ controllightsonoff/controllightsonoff.ino
@@ -0,0 +1,200 @@
+#include <WiFi.h>
+#include <Espalexa.h>
+#include <AceButton.h>
+using namespace ace_button;
+
+Espalexa espalexa;
+
+// define the GPIO connected with Relays and switches
+#define RelayPin1 23  //D23
+#define RelayPin2 22  //D22
+#define RelayPin3 21  //D21
+#define RelayPin4 19  //D19
+#define wifiLed    2   //D2
+
+// WiFi Credentials
+const char* ssid = "A.T.O.M_LABS";
+const char* password = "Atom281121";
+
+// device names
+String Device_1_Name = "Lamp1";
+String Device_2_Name = "Fan";
+String Device_3_Name = "Lamp3";
+String Device_4_Name = "Lamp4";
+
+// prototypes
+boolean connectWifi();
+
+//callback functions
+void firstLightChanged(uint8_t brightness);
+void secondLightChanged(uint8_t brightness);
+void thirdLightChanged(uint8_t brightness);
+void fourthLightChanged(uint8_t brightness);
+
+boolean wifiConnected = false;
+
+//our callback functions
+void firstLightChanged(uint8_t brightness)
+{
+  //Control the device
+  if (brightness == 255)
+    {
+      digitalWrite(RelayPin1, LOW);
+      Serial.println("Device1 ON");
+    }
+  else
+  {
+    digitalWrite(RelayPin1, HIGH);
+    Serial.println("Device1 OFF");
+  }
+}
+
+void secondLightChanged(uint8_t brightness)
+{
+  //Control the device 
+  if (brightness == 255)
+    {
+      digitalWrite(RelayPin2, LOW);
+      Serial.println("Device2 ON");
+    }
+  else
+  {
+    digitalWrite(RelayPin2, HIGH);
+    Serial.println("Device2 OFF");
+  }
+}
+
+void thirdLightChanged(uint8_t brightness)
+{
+  //Control the device  
+  if (brightness == 255)
+    {
+      digitalWrite(RelayPin3, LOW);
+      Serial.println("Device3 ON");
+    }
+  else
+  {
+    digitalWrite(RelayPin3, HIGH);
+    Serial.println("Device3 OFF");
+  }
+}
+
+void fourthLightChanged(uint8_t brightness)
+{
+  //Control the device 
+  if (brightness == 255)
+    {
+      digitalWrite(RelayPin4, LOW);
+      Serial.println("Device4 ON");
+    }
+  else
+  {
+    digitalWrite(RelayPin4, HIGH);
+    Serial.println("Device4 OFF");
+  }
+}
+
+
+
+// connect to wifi  returns true if successful or false if not
+boolean connectWifi()
+{
+  boolean state = true;
+  int i = 0;
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+  Serial.println("Connecting to WiFi");
+
+  // Wait for connection
+  Serial.print("Connecting...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    if (i > 20) {
+      state = false; break;
+    }
+    i++;
+  }
+  Serial.println("");
+  if (state) {
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+  }
+  else {
+    Serial.println("Connection failed.");
+  }
+  return state;
+}
+
+void addDevices(){
+  // Define your devices here.
+  espalexa.addDevice(Device_1_Name, firstLightChanged); //simplest definition, default state off
+  espalexa.addDevice(Device_2_Name, secondLightChanged);
+  espalexa.addDevice(Device_3_Name, thirdLightChanged);
+  espalexa.addDevice(Device_4_Name, fourthLightChanged);
+  
+  espalexa.begin();
+}
+
+void setup()
+{
+  Serial.begin(115200);
+
+  pinMode(RelayPin1, OUTPUT);
+  pinMode(RelayPin2, OUTPUT);
+  pinMode(RelayPin3, OUTPUT);
+  pinMode(RelayPin4, OUTPUT);
+
+  pinMode(wifiLed, OUTPUT);
+
+  //During Starting all Relays should TURN OFF
+  digitalWrite(RelayPin1, HIGH);
+  digitalWrite(RelayPin2, HIGH);
+  digitalWrite(RelayPin3, HIGH);
+  digitalWrite(RelayPin4, HIGH);
+
+
+
+  // Initialise wifi connection
+  wifiConnected = connectWifi();
+
+  if (wifiConnected)
+  {
+    addDevices();
+  }
+  else
+  {
+    Serial.println("Cannot connect to WiFi. So in Manual Mode");
+    delay(1000);
+  }
+}
+
+void loop()
+{
+   if (WiFi.status() != WL_CONNECTED)
+  {
+    //Serial.print("WiFi Not Connected ");
+    digitalWrite(wifiLed, LOW); //Turn off WiFi LED
+  }
+  else
+  {
+    //Serial.print("WiFi Connected  ");
+    digitalWrite(wifiLed, HIGH);
+    //Manual Switch Control
+    //WiFi Control
+    if (wifiConnected){
+      espalexa.loop();
+      delay(1);
+    }
+    else {
+      wifiConnected = connectWifi(); // Initialise wifi connection
+      if(wifiConnected){
+      addDevices();
+      }
+    }
+  }
+}
